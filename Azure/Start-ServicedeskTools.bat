7cc79796c41970ce493fe20d7e55e0b2823fdb7b
Alexander Hagen <mrhagen98@outlook.com>
2026-01-23 22:57:48 +0100

    Starting tool for SD Tools

--- Azure/Start-ServicedeskTools.bat
+++ Azure/Start-ServicedeskTools.bat
@@ -1,7 +1,4 @@
 @echo off
-REM CMCS Servicedesk Tools Launcher
-REM Double-click this file to start the Servicedesk Tools
-REM Made by Mr. Hagen - 2026
 
 cd /d "%~dp0"
 powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Tools.ps1"
